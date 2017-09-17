c3aeeedb3bdc3d4f4eb5d767db34dc2fa937c58f
Adam Pruner <adampruner99@gmail.com>
2017-09-16 21:24:26 -0700

    added send button

--- src/components/ChatInputField.vue
+++ src/components/ChatInputField.vue
@@ -3,6 +3,7 @@
   <div class="chatinputfield">
     <div class="field">
       <textarea class="textarea" placeholder="Type your message here!"></textarea>
+      <a class="button is-info">Send message</a>
     </div>
   </div>
 </template>
@@ -11,18 +12,24 @@
   export default {
     name: 'chatinputfield',
     data: {
-
     }
   }
 </script>
 
 <style lang="scss">
   @import '~bulma/sass/utilities/variables';
-  .textarea {
+  .field {
     position: fixed;
     min-width: 98%;
     max-width: 98%;
     bottom: 0;
+  }
+  .field .button {
+    position: relative;
+    float: right;
+    margin-bottom: 5px;
+  }
+  .textarea {
     margin: 10px;
     padding: 5px;
     box-shadow: 0 2px 3px rgba(17, 17, 17, 0.1), 0 0 0 1px rgba(17, 17, 17, 0.1);
