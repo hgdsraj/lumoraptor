2fc2561aa4f08e940c828b483645b52e1e35983d
Adam Pruner <adampruner99@gmail.com>
2017-09-16 18:47:34 -0700

    Added chatwindow link and component, no data yet

---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,26 @@
+<!-- Vue component for chat window -->
+<template>
+  <div class="chatwindow">
+    <div class="tile is-ancestor is-12 is-large">
+     Ye
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'chatwindow'
+  }
+</script>
+
+<style lang="scss">
+  @import '~bulma/sass/utilities/variables';
+  .chatwindow {
+    .tile {
+      margin: 10px;
+      padding: 10px;
+      box-shadow: 0 2px 3px rgba(17, 17, 17, 0.1), 0 0 0 1px rgba(17, 17, 17, 0.1);
+      background-color: turquoise;
+    }
+  }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -2,20 +2,14 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
 import Login from '@/components/Login'
+import ChatWindow from '@/components/ChatWindow'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/login',
-      name: 'Login',
-      component: Login
-    }
+    { path: '/', component: Home },
+    { path: '/login', component: Login }
+    { path: '/chatwindow', component: ChatWindow }
   ]
 })
